41855f1cda0b37780393fb2a7ec1595b0c74486d
AndreaVerweijmeren <61025668+AndreaVerweijmeren@users.noreply.github.com>
2020-04-02 23:40:21 +0200

    finished app
    Might be bugged when downloaded due to github changing capital letters

---
+++ Owner side/Create animal profile/JS/CAP.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+
+
+function previewFile(){
+  var preview = document.querySelector('img'); //selects the query named img
+  var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file); //reads the data as a URL
+  } 
+}
+
+  previewFile();  //calls the function named previewFile()
+
+
+/* BOOTSTRAP DROPDOWN MENU - Update selected item text and image */
+$(".dropdown-menu li a").click(function () {
+    var selText = $(this).text();
+    var imgSource = $(this).find('img').attr('src');
+    var img = '<img src="' + imgSource + '"/>';        
+    $(this).parents('.btn-group').find('.dropdown-toggle').html(img + ' ' + selText + ' <span class="caret"></span>');
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
